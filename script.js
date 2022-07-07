b0a400a2bb13e5dd7ca488503859c8170066107d
Max Clendon <108703092+maxTheSlug@users.noreply.github.com>
2022-07-07 21:41:30 +0000

    movie05.js

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ var userName;
 var userAge;
 //Number variable that saves the user's scare limit
 var userScareLimit;
-//Boolean variable that is used in error message while loops; while invalidValue = true, etc. NOT NEEDED.
-//var invalidValue = true;
+//Boolean variable that is used in error message while loops; while invalidValue = true, etc.
+var confirmMovie = true;
+
 
 //Main code
 function start() {
@@ -34,19 +35,8 @@ function start() {
   CheckUserAge();
   //Lists the avalible movies in the order shown on the task sheet, 0,4,2,1,3,5
   alert("Welcome to the movie selector program " + userName + ", you are " + userAge + " years old.");
-  alert("As this is a rainy Sunday afternoon, you might like to watch a scary movie. \nHere is a list of available scary movies. \n" + MOVIENAME[0] + " \n" + MOVIENAME[4] + " \n" + MOVIENAME[2] + " \n" + MOVIENAME[1] + " \n" + MOVIENAME[3] + " \n" + MOVIENAME[5]);
-  //Asks for and store's the user's prefered scare level in the userScareLimit variable
-  userScareLimit = prompt("From " + LOWSCARE + " to " + HIGHSCARE + ", what is the highest level of scariness you would like for your movie?");
-  //Checks if the user has entered a valid scare limit, a number between 1 and 10.
-  CheckUserScareLimit();
-  //array something
-  for (i = 0; i < MOVIENAME.length; i++) {
-    if (userScareLimit >= MOVIESCARE[i]) {
-      alert("Hi " + userName + ", age " + userAge + ", based on your preferred level of scariness, we think that " + MOVIENAME[i] + " is the best movie choice for you.");
-    break;
-    }
-  }
-
+  SuggestUserMovie();
+  
 }
   
 //Functions
@@ -68,5 +58,26 @@ function CheckUserScareLimit() {
   while (userScareLimit < LOWSCARE || userScareLimit > HIGHSCARE || isNaN(userScareLimit)) {
   userScareLimit = prompt("Sorry, '" + userScareLimit + "' is not a valid scare limit. Please enter a scare limit that is between " + LOWSCARE + " and " + HIGHSCARE + ". ");
   }
-  
+}
+function SuggestUserMovie() {
+  alert("As this is a rainy Sunday afternoon, you might like to watch a scary movie. \nHere is a list of available scary movies. \n" + MOVIENAME[0] + " \n" + MOVIENAME[4] + " \n" + MOVIENAME[2] + " \n" + MOVIENAME[1] + " \n" + MOVIENAME[3] + " \n" + MOVIENAME[5]);
+  //Asks for and store's the user's prefered scare level in the userScareLimit variable
+  userScareLimit = prompt("From " + LOWSCARE + " to " + HIGHSCARE + ", what is the highest level of scariness you would like for your movie?");
+  //Checks if the user has entered a valid scare limit, a number between 1 and 10.
+  CheckUserScareLimit();
+  //array something
+  for (i = 0; i < MOVIENAME.length; i++) {
+    if (userScareLimit >= MOVIESCARE[i]) {
+      alert("Hi " + userName + ", age " + userAge + ", based on your preferred level of scariness, we think that " + MOVIENAME[i] + " is the best movie choice for you.");
+    break;
+    }
+  }
+  confirmMovie = confirm("Please confirm that the movie you would like to watch is " + MOVIENAME[i] + ". This movie has a scariness level of " + MOVIESCARE[i] + ". This is within your prefered scariness level, but if you would like to change your mind, press Cancel. If you would like to break and watch " + MOVIENAME[i] + ", press Ok.");
+  switch (true) {
+    case (confirmMovie == true):
+      alert("Starting " + MOVIENAME[i] + ".");
+      break;
+    case (confirmMovie == false):
+      SuggestUserMovie();
+  }
 }
